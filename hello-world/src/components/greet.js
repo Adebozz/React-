0671cf90c5e6d70795077aa714a5af3853ecb9c4
Adebozz <ademolaadeigbe3@gmail.com>
2023-01-31 23:59:40 +0100

    update

--- hello-world/src/components/greet.js
+++ hello-world/src/components/greet.js
@@ -6,6 +6,6 @@ import React from 'react'
 const Greet = props => {
    const {name, job} = props
 return<body><h1>Hello {props.name}</h1>
-<h2>I am a {props.job} and I love my job</h2></body>
+<h2>I am a {props.job} I love my job and i love working</h2></body>
 }
 export default Greet
