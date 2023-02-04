b7506e5a46e4b74bf20428bf9a7b3638f2240c72
Adebozz <ademolaadeigbe3@gmail.com>
2023-02-04 18:00:34 +0100

    third commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -13,11 +13,17 @@ import ParentComponent from './components/parentComponent';
 import UserGreeting from './components/UserGreeting';
 import PersonList from './components/nameList';
 import Stylesheet from './components/Stylesheet';
+import Inline from './components/inline';
+import './appStyle.css'
+import styles from './appStyle.module.css'
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <Stylesheet />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <Inline /> */}
+      {/* <Stylesheet primary={false} /> */}
       {/* <PersonList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
--- hello-world/src/components/Stylesheet.js
+++ hello-world/src/components/Stylesheet.js
@@ -1,10 +1,11 @@
 import React from "react"
 import './myStyle.css'
 
-function Stylesheet() {
+function Stylesheet(props) {
+    let className = props.primary ? 'primary' : ''
     return (
         <div>
-            <h1 className="primary">Stylesheets</h1>
+            <h1 className={`${className} font-xl`}>Stylesheets</h1>
         </div>
     )
 }
